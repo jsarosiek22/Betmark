486c05be6fe81c909a87c3ecfe91b30e141b10ee
Jacob Sarosiek <jsarosiek22@gmail.com>
2020-03-01 20:42:28 -0600

    Start learning ReactJS

--- betmark/src/App.js
+++ betmark/src/App.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
+class Header extends Component {
+  render() {
+    return (
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-		<p>
-			Test
-		</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">{this.props.title}</h1>
       </header>
-    </div>
-  );
+    );
+  }
+}
+class Body extends Component {
+  render() {
+    return <div>testing</div>;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header title={"Betmark"} />
+        <Body />
+      </div>
+    );
+  }
 }
 
 export default App;
