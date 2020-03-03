3bc3910797d0a5898475a05ffbc41629e5dcc69e
Jacob Sarosiek <jsarosiek22@gmail.com>
2020-03-02 21:52:12 -0600

    Add mongoDB

---
+++ betmark/src/DB.js
@@ -0,0 +1,69 @@
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://jacobsar:884eVabYm@cluster0-izhej.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+async function asyncCall() {
+  // connect to your cluster
+  const client = await MongoClient.connect(
+    "mongodb+srv://jacobsar:884eVabYm@cluster0-izhej.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
+  // specify the DB's name
+  const db = client.db("users");
+  // execute find query
+  /* Insert Example */
+  //   var myObj = {
+  //     ID: 100000,
+  //     FirstName: "Kelly",
+  //     LastName: "Sarosiek",
+  //     UserID: "kellys",
+  //     Pwd: "ks"
+  //   };
+  //   await db.collection("users").insertOne(myObj, function(err, res) {
+  //     console.log(res);
+  //   });
+
+  /* Update Example */
+  //   var myquery = { FirstName: "Kelly" };
+  //   var newvals = { $set: { ID: 100001 } };
+  //   await db.collection("users").updateOne(myquery, newvals, function(err, res) {
+  //     console.log(res);
+  //   });
+
+  const items = await db
+    .collection("users")
+    .find({})
+    .toArray();
+  console.log(items);
+  // close connection
+  client.close();
+}
+
+async function asyncCall2() {
+  client.connect(err => {
+    const collection = client.db("users").collection("users");
+    if (err) throw err;
+    var myObj = {
+      ID: 100000,
+      FirstName: "Kelly",
+      LastName: "Sarosiek",
+      UserID: "kellys",
+      Pwd: "ks"
+    };
+    collection.insertOne(myObj, function(err, res) {
+      if (err) throw err;
+      console.log(res);
+      db.close();
+    });
+
+    client.close();
+  });
+}
+
+asyncCall();
